4572e79691d54d414afb1e85e50263e3aed9d906
mousa nageh <200moussa200@gmail.com>
2024-03-21 18:06:43 +0200

    add examples for array data structure

---
+++ 2-data_structure/1-arrays/3-examples.py
@@ -0,0 +1,35 @@
+# revsere a string 
+
+def reverse(string: str):
+    if not string or len(string) < 2:
+        return string
+    res = ""
+    for i in range(len(string) -1 , -1, -1):
+        res += string[i]
+    return res 
+
+
+
+def reverse2(string: str):
+    if not string or len(string) < 2:
+        return string
+    res = list(string)
+    i = 0 
+    j = len(res) -1 
+    while i < j:
+        res[i], res[j] = res[j], res[i]
+        i +=1
+        j -=1
+    return "".join(res)
+
+
+def reverse3(string: str):
+    return string[::-1]
+
+
+def reverse4(string: str):
+    list_val = [*string]
+    list_val.reverse()
+    return "".join(list_val)
+
+print(reverse('mousa') == reverse2('mousa') == reverse3('mousa')== reverse4('mousa')) 
