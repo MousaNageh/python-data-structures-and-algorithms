2ad8921fdfd651d2078cfb4afe9463e393bb4ad9
mousa nageh <200moussa200@gmail.com>
2024-03-22 00:10:24 +0200

    implement hash table examples

--- 2-data_structure/2-hash_tables/4-example_first_occurrence.py
+++ 2-data_structure/2-hash_tables/4-example_first_occurrence.py
@@ -10,16 +10,17 @@
 
 from typing import List
 
-# O(n^2)
-
-def find_first_occurence(arr: List[int]):
+# O(n^2) time complexity
+#O(1) for space complexity
+def find_first_occurence1(arr: List[int]):
     for i in range(len(arr)):
         for j in range(i+1, len(arr)):
             if arr[i] == arr[j]:
                 return arr[i]
 
-#O(n)
-def find_first_occurence(arr: List[int]):
+#O(n) for time complexity
+#O(n) for space complexity
+def find_first_occurence2(arr: List[int]):
     
     if not arr or len(arr) < 2:
         return None
@@ -30,7 +31,24 @@ def find_first_occurence(arr: List[int]):
         else:
             occurence.add(value)
 
+# O(n^2) time complexity
+#O(1) for space complexity
+def find_first_occurence3(arr: List[int]):
+    small_index_sum = len(arr)
+    matches = None
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                if i + j <= small_index_sum:
+                    small_index_sum =  i + j 
+                    matches = arr[i]
+    return matches
 
-print(find_first_occurence([2,5,1,2,3,5,1,2,4]))
-print(find_first_occurence([2,1,1,2,3,5,1,2,4]))
-print(find_first_occurence([2,3,4,5]))
+print(find_first_occurence1([2,5,1,2,3,5,1,2,4]) == find_first_occurence2([2,5,1,2,3,5,1,2,4]) == find_first_occurence3([2,5,1,2,3,5,1,2,4])) # true
+print(find_first_occurence1([2,1,1,2,3,5,1,2,4]) == find_first_occurence2([2,1,1,2,3,5,1,2,4]) ) # false 
+print(find_first_occurence1([2,1,1,2,3,5,1,2,4])) # this will return 2 
+print(find_first_occurence2([2,1,1,2,3,5,1,2,4])) # this will return 1 
+print(find_first_occurence3([2,1,1,2,3,5,1,2,4])) # this will return 1 
+print(find_first_occurence1([2,3,4,5]) == find_first_occurence2([2,3,4,5]) == find_first_occurence3([2,3,4,5])) # true
+
+
